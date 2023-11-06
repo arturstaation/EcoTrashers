92c308f7adb69c4e9397f14c9130cd00a29549ee
Staation <arturmackenzie12@gmail.com>
2023-11-06 17:36:41 -0300

    Adicionado sistema de highscore e armazenar pontos (sistema de moedas)

--- app/src/main/java/com/example/ihc/MainActivity.java
+++ app/src/main/java/com/example/ihc/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ihc;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -16,17 +18,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     int vidas;
-    int pontos;
+    long pontos;
 
     int lixo;
-
-
-
+    private SharedPreferences getHigh;
+    private SharedPreferences getMoeda;
+    private long highscore;
+    private long moedas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getHigh = getSharedPreferences("highscore", Context.MODE_PRIVATE);
+        getMoeda = getSharedPreferences("moedas", Context.MODE_PRIVATE);
+
+        highscore = getHigh.getLong("highscore", 0L);
+        moedas = getMoeda.getLong("moedas", 0L);
     }
 
     public void onJogar(View view) {
@@ -103,8 +112,32 @@ public class MainActivity extends AppCompatActivity {
                 // Verifique se o jogo acabou
                 if (vidas <= 0) {
                     setContentView(R.layout.activity_perder);
-                    TextView derrota = findViewById(R.id.texto_derrota);
+                    TextView derrota = findViewById(R.id.texto_highscore);
                     derrota.setText("Sua pontuacao foi: " + pontos);
+                    moedas = moedas + pontos;
+                    SharedPreferences.Editor editMoedas = getMoeda.edit();
+                    editMoedas.putLong("moedas", moedas);
+                    editMoedas.apply();
+
+                    TextView pmaxima = findViewById(R.id.texto_pontuacaomaxima);
+                    TextView saldo = findViewById(R.id.texto_saldo);
+                    saldo.setText("Seu saldo atual é de: " + moedas);
+
+                    if(pontos > highscore){
+                        highscore = pontos;
+                        SharedPreferences.Editor editHigh = getHigh.edit();
+                        editHigh.putLong("highscore", highscore);
+                        editHigh.apply();
+                        pmaxima.setText("Parabens voce bateu seu recorde! Novo recorde: " + highscore);
+                    }else{
+
+                        pmaxima.setText("Seu recorde é: " + highscore);
+                    }
+
+
+
+
+
 
                 }
             }
@@ -120,6 +153,15 @@ public class MainActivity extends AppCompatActivity {
     public void onPerder(View view){
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPerfil(View view){
+
+        setContentView(R.layout.activity_perfil);
+        TextView texto_highscore = findViewById(R.id.texto_highscore);
+        TextView texto_saldo = findViewById(R.id.texto_saldo);
+        texto_highscore.setText("Seu recorde é: " + highscore);
+        texto_saldo.setText("Seu saldo é: " + moedas);
     }
 
 
